54db9745b1a3c9c28fe1e243e624396e7d6109a6
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2024-12-25 16:47:51 +0530

    Added various Mongoose models for user, video, comment, like, tweet, playlist, subscription and healthcheck API

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// import routes
+import healthcheckRouter from "./routes/healthcheck.router.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
 export { app };
